d20ce2c60d2d7f3ad748718d06aa6e7544375c54
budibrass <immortalbudi@yahoo.com>
2023-03-24 00:36:20 +0700

    penyesuaian pada require

--- 1.js
+++ 1.js
@@ -1,16 +1,28 @@
 function FindMaxNumber(array) {
-  let numberMax = 0;
-  let i = 0;
-  while (i < array.length) {
-    let conOnes = 0;
-    while (i < array.length && array[i] == 1) {
-      conOnes++;
+  let dataCheck = [];
+  array.map((e) => {
+    if (e !== 0 && e !== 1) {
+      dataCheck.push(e);
+    }
+  });
+  if (array.length > 10000 || dataCheck.length > 0) {
+    console.log(
+      `Maaf array yang di input hanya boleh angka 0 dan 1 dan tidak boleh melebihi 10.000 karakter`
+    );
+  } else {
+    let numberMax = 0;
+    let i = 0;
+    while (i < array.length) {
+      let conOnes = 0;
+      while (i < array.length && array[i] == 1) {
+        conOnes++;
+        i++;
+      }
+      numberMax = Math.max(numberMax, conOnes);
       i++;
     }
-    numberMax = Math.max(numberMax, conOnes);
-    i++;
+    console.log(numberMax);
   }
-  console.log(numberMax);
 }
 
-FindMaxNumber([1,0,0,1,0,1,1]);
+FindMaxNumber([1, 0, 0, 1, 0, 1, 1]);
